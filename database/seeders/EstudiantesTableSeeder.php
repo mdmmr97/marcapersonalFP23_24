8e94ec8ab161a9652ebe8e74fa9b37d4e76a85ee
mdmmr97 <3730156@alu.murciaeduca.es>
2023-11-30 09:04:08 +0100

    Explicacion factories

---
+++ database/seeders/EstudiantesTableSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Estudiante;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class EstudiantesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        Estudiante::truncate();
+        Estudiante::factory(20)->create();
+
+        /*$estudiante = new Estudiante;
+        $estudiante->nombre = 'Juan';
+        $estudiante->apellidos = 'Martínez';
+        $estudiante->direccion = 'Dirección de Juan';
+        $estudiante->votos = 130;
+        $estudiante->confirmado = true;
+        $estudiante->ciclo = 'DAW';
+        $estudiante->save();*/
+    }
+}
